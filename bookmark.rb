2a435215cabef0bba5ae3db2f113dbc10707b991
Wellson Fung <wellson.fung@gmail.com>
2015-04-08 23:33:13 -0700

    adding bookmark.rb homework

---
+++ bookmark.rb
@@ -0,0 +1,16 @@
+class Bookmark
+  attr_accessor :url, :title
+  attr_reader :last_visited
+
+  def initialize(url, title)
+    @url = url
+    @title = title
+    @last_visited = self.visit!
+  end
+
+  def visit!
+    Time.now
+  end
+end
+
+puts bookmark = Bookmark.new('www.google.com', 'google')
